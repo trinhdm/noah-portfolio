932d0de7df0d98f9e2954c8f257d6a1c441f3ba9
Doreen Trinh <doreenmtrinh@gmail.com>
2025-10-01 13:14:08 -0700

    refactor: created separate functions per header event

--- src/global/header/header.ts
+++ src/global/header/header.ts
@@ -1,72 +1,98 @@
-
-
 import { setAnimation } from '../../utils/css'
 
 
-export const handleHeader = () => {
-	const headerNav = document.querySelector('.header-nav') as HTMLElement | null,
-		navList = headerNav?.querySelector('.header-nav-list') as HTMLElement | null,
-		navItems = headerNav?.querySelectorAll('.header-nav-item') as NodeListOf<HTMLElement>
+type NavEventOptions = {
+	args: {
+		duration: number
+		stagger: number
+	}
+	navList: HTMLElement
+	timer: NodeJS.Timeout
+}
 
-
-	if (!headerNav || !navList) return
-
+const onLoadHeader = (headerNav: HTMLElement) => {
 	headerNav.dataset.loading = 'true'
 
 	headerNav.addEventListener('animationend', () => {
 		delete headerNav.dataset.loading
 	}, { passive: true })
+}
 
-	const argsAnimate = { duration: .25 },
-		isAnimatingClass = 'header-nav--animated',
-		stagger = -0.1
-	let hoverTimer: NodeJS.Timeout
+const resetAnimation = (element: HTMLElement | null, className = 'header-nav--animated'): void => {
+	if (!element) return
+	element.classList.remove(className)
+	void element.offsetHeight		// trigger reflow
+	element.classList.add(className)
+}
 
-	const resetAnimation = (element: HTMLElement | null = navList): void => {
-		if (!element) return
-		element.classList.remove(isAnimatingClass)
-		void element.offsetHeight		// trigger reflow
-		element.classList.add(isAnimatingClass)
-	}
+const onEnterNav = ({
+	args,
+	navList,
+	timer,
+}: NavEventOptions) => {
+	const navItems = navList.querySelectorAll('.header-nav-item') as NodeListOf<HTMLElement>
+
+	navItems.forEach((item, i) => Object.assign(
+		item?.style, setAnimation({
+			...args,
+			index: i,
+			start: Math.abs(args.stagger * navItems.length),
+		})
+	))
+
+	clearTimeout(timer)
+
+	timer = setTimeout(() => {
+		navList.classList.add('header-nav--active')
+		resetAnimation(navList)
+	}, 100)
+}
+
+const onLeaveNav = ({
+	args,
+	navList,
+	timer,
+}: NavEventOptions) => {
+	const navItems = navList.querySelectorAll('.header-nav-item') as NodeListOf<HTMLElement>
+
+	navItems.forEach((item, i) => Object.assign(
+		item?.style, setAnimation({
+			...args,
+			index: i,
+			stagger: Math.abs(args.stagger),
+		})
+	))
+
+	clearTimeout(timer)
+
+	timer = setTimeout(() => {
+		navList.classList.remove('header-nav--active')
+		resetAnimation(navList)
+
+		;([...navItems].at(-1))?.addEventListener('animationend', event => {
+			event.stopPropagation()
+			navList.classList.remove('header-nav--animated')
+			console.log('END ANIMATION')
+		}, { once: true, passive: true })
+	}, 100)
+}
+
+export const handleHeader = () => {
+	const headerNav = document.querySelector('.header-nav') as HTMLElement | null,
+		navList = headerNav?.querySelector('.header-nav-list') as HTMLElement | null
+
+	if (!headerNav || !navList) return
+
+	const args = { duration: .25, stagger: -0.1 }
+	let timer: NodeJS.Timeout
+
+	onLoadHeader(headerNav)
 
 	navList.addEventListener('mouseenter', () => {
-		navItems.forEach((item, i) => Object.assign(
-			item?.style, setAnimation({
-				...argsAnimate,
-				index: i,
-				stagger,
-				start: Math.abs(stagger * navItems.length),
-			})
-		))
-
-		clearTimeout(hoverTimer)
-
-		hoverTimer = setTimeout(() => {
-			navList.classList.add('header-nav--active')
-			resetAnimation()
-		}, 100)
+		onEnterNav({ args, navList, timer })
 	}, { passive: true })
 
 	navList.addEventListener('mouseleave', () => {
-		navItems.forEach((item, i) => Object.assign(
-			item?.style, setAnimation({
-				...argsAnimate,
-				index: i,
-				stagger: Math.abs(stagger),
-			})
-		))
-
-		clearTimeout(hoverTimer)
-
-		hoverTimer = setTimeout(() => {
-			navList.classList.remove('header-nav--active')
-			resetAnimation()
-
-			;([...navItems].at(-1))?.addEventListener('animationend', event => {
-				event.stopPropagation()
-				navList.classList.remove(isAnimatingClass)
-				console.log('END ANIMATION')
-			}, { once: true, passive: true })
-		}, 100)
+		onLeaveNav({ args, navList, timer })
 	}, { passive: true })
 }
