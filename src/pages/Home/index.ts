5260277ce51992a2c9d8878c77d419582b11ea00
Doreen Trinh <doreenmtrinh@gmail.com>
2025-10-19 06:20:19 -0700

    feat: Home page blocks

---
+++ src/pages/Home/index.ts
@@ -0,0 +1,33 @@
+import { BlockHome } from './BlockHome.ts'
+import './home.css'
+
+
+const Home = {
+	init: () => {
+		const blocks = document.querySelectorAll('.sqs-block') as NodeListOf<HTMLElement>
+		const className = 'home-block'
+
+
+		blocks.forEach((block: HTMLElement, index: number) => {
+			const blockEl = BlockHome.init({
+				className,
+				index,
+				target: block,
+			})
+
+			if (!blockEl) return
+
+			block.addEventListener('click', (event: MouseEvent) => {
+				event.preventDefault()
+				event.stopPropagation()
+				console.log(block)
+				// createLightbox(blockEl)
+			})
+		})
+
+		// styleBlockTypography({ className })
+	}
+}
+
+
+export { Home }
