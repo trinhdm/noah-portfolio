cd930a1593cbaf70ae1891694a388d7c111ad788
Doreen Trinh <doreenmtrinh@gmail.com>
2025-10-19 06:20:34 -0700

    feat: About page blocks

---
+++ src/pages/About/index.ts
@@ -0,0 +1,33 @@
+import { BlockAbout } from './BlockAbout.ts'
+import './about.css'
+
+
+const About = {
+	init: () => {
+		const blocks = document.querySelectorAll('.sqs-block') as NodeListOf<HTMLElement>
+		const className = 'about-block'
+
+
+		blocks.forEach((block: HTMLElement, index: number) => {
+			const blockEl = BlockAbout.init({
+				className,
+				index,
+				target: block,
+			})
+
+			if (!blockEl) return
+
+			block.addEventListener('click', (event: MouseEvent) => {
+				event.preventDefault()
+				event.stopPropagation()
+				console.log(block)
+				// createLightbox(blockEl)
+			})
+		})
+
+		// styleBlockTypography({ className })
+	}
+}
+
+
+export { About }
