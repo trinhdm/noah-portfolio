6712ebe68d1aa1ddd86e39c50b9ecdf8004a8bdf
Doreen Trinh <doreenmtrinh@gmail.com>
2025-10-31 08:20:19 -0700

    refactor: added wrapper around each lightbox control

--- src/components/Lightbox/template.ts
+++ src/components/Lightbox/template.ts
@@ -11,17 +11,21 @@ const template = `
 		</div>
 		<div class="lightbox__footer">
 			<div class="lightbox__navigation">
-				<div class="lightbox__control" data-direction="prev">
-					<button class="lightbox__icon" data-icon="arrow-prev">
-						<span>&lsaquo;</span>
-					</button>
-					<span class="lightbox__label"></span>
+				<div class="lightbox__navitem" data-direction="prev">
+					<div class="lightbox__control">
+						<button class="lightbox__icon" data-icon="arrow-prev">
+							<span>&lsaquo;</span>
+						</button>
+						<span class="lightbox__label"></span>
+					</div>
 				</div>
-				<div class="lightbox__control" data-direction="next">
-					<span class="lightbox__label"></span>
-					<button class="lightbox__icon" data-icon="arrow-next">
-						<span>&rsaquo;</span>
-					</button>
+				<div class="lightbox__navitem" data-direction="next">
+					<div class="lightbox__control">
+						<span class="lightbox__label"></span>
+						<button class="lightbox__icon" data-icon="arrow-next">
+							<span>&rsaquo;</span>
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
